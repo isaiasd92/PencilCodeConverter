22c524c75bd835db9465bf6def57bf2aac84e5a7
Isaias Delgado <isaiasdelgado03@gmail.com>
2017-03-23 20:47:27 -0500

    Custom library to convert PencilCode for the EV3

---
+++ PencilCodeConverter/PencilCodeConverter/FileConversion.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PencilCodeConverter
+{
+    class Files
+    {
+        string[] NXC = new string[100];
+        string[] wordArray = new string[100];
+        int for_rev = 10000;
+        int left_right = 750;
+        int j = 0;
+
+        MainWindow main = new MainWindow();
+
+        public void AddToNXCArray(string _com)
+        {
+            NXC[j] = _com;
+            j++;
+        }
+
+        public void AddToNXCArray(string _com1, string _com2, string _com3, int _com4, string _com5)
+        {
+            string _com = (_com1 + _com2 + _com3 + _com4.ToString() + _com5).ToString();
+            NXC[j] = _com;
+            j++;
+        }
+
+        public void FileConversion(string _fileAddress)
+        {
+            string commands = System.IO.File.ReadAllText(_fileAddress);
+            string words = System.Text.RegularExpressions.Regex.Replace(commands, @"\s+", Environment.NewLine);
+
+            AddToNXCArray("#define right(s,t) OnFwd(OUT_A, s); OnRev(OUT_B, s); Wait(t);");
+            AddToNXCArray("#define left(s,t) OnRev(OUT_A, s); OnFwd(OUT_B, s); Wait(t);");
+            AddToNXCArray("#define forward(s,t) OnFwd(OUT_AB, s); Wait(t);");
+            AddToNXCArray("#define reverse(s,t) OnRev(OUT_AB, s); Wait(t);");
+            AddToNXCArray("\ntask main()");
+            AddToNXCArray("{");
+
+            wordArray = words.Split('\n');
+
+            for (int i = 0; i < wordArray.Length; i++)
+            {
+                if (wordArray[i].Contains("fd"))
+                    AddToNXCArray("\tforward(", wordArray[i + 1], ",", for_rev, ");\n");
+
+                if (wordArray[i].Contains("bk"))
+                    AddToNXCArray("\treverse(", wordArray[i + 1], ",", for_rev, ");\n");
+
+                if (wordArray[i].Contains("rt"))
+                    AddToNXCArray("\tright(", wordArray[i + 1], ",", left_right, ");\n");
+
+                if (wordArray[i].Contains("lt"))
+                    AddToNXCArray("\tleft(", wordArray[i + 1], ",", left_right, ");\n");
+            }
+
+            AddToNXCArray("\tOff(OUT_AB);");
+            AddToNXCArray("}");
+
+            string robotCode = @"C:\Users\Owner\Desktop\robotCode.nxc";
+            main.FileExists(robotCode, NXC);
+            File.Delete(main.FileName);
+        }
+    }
+}
---
+++ PencilCodeConverter/PencilCodeConverter/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PencilCodeConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string outFile = @"C:\Users\Owner\Desktop\Data.txt";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string FileName
+        {
+            get { return outFile; }
+        }
+
+        private void URL_Conversion()
+        {
+            //string old_url = PencilCode_URL.Text;
+            string old_url = "http://batmanizzy2.pencilcode.net/edit/intro";
+            /*    ^^^^ REMOVE ^^^^    */
+            string new_url = old_url.Replace("edit", "code");
+            New_URL_TextBlock.Text = new_url;
+        }
+
+        public void DownloadFile()
+        {
+            WebClient client = new WebClient();
+            string text = client.DownloadString(New_URL_TextBlock.Text);
+            FileExists(text);
+        }
+
+        /*
+        public static bool CheckNum(string _word)
+        {
+            
+            //int num;
+            //return Int32.TryParse(_word, out num);
+            
+            foreach(char c in _word)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+            return true;
+        }
+        */
+
+        public void FileExists(string _data)
+        {
+            if (File.Exists(outFile))
+            {
+                File.Delete(outFile);
+                FileExists(_data);
+            }
+            File.WriteAllText(outFile, _data);
+        }
+
+        public void FileExists(string _file, string[] _data)
+        {
+            if (File.Exists(_file))
+            {
+                File.Delete(_file);
+                FileExists(_file, _data);
+            }
+            File.WriteAllLines(_file, _data);
+        }
+
+        private void PencilCode_Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://pencilcode.net/edit/first");
+        }
+
+        private void PencilCode_URL_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            //MessageBox.Show("Enter PencilCode Project URL Here");
+            tb.Text = string.Empty;
+            tb.GotFocus -= PencilCode_URL_GotFocus;
+        }
+
+        private void Enter_Button_Click(object sender, RoutedEventArgs e)
+        {
+            URL_Conversion();
+            Download_Button.IsEnabled = true;
+        }
+
+        private void Download_Button_Click(object sender, RoutedEventArgs e)
+        {
+            DownloadFile();
+            Files file = new Files();
+            file.FileConversion(outFile);
+        }
+    }
+}
